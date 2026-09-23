e3a9e2b4579f04719fb45cf1e16def1c84a97adc
Ridge <79848350+ridgeywtf@users.noreply.github.com>
2026-09-23 20:40:27 +0100

    Fix API endpoint to use search_on variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         
         # The course leaves this as fruit_chosen for now (showing "Not found" for Apples is expected at this stage)
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
